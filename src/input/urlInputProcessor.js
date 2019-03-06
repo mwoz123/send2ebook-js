d8fe1627e59bc899a2eb75232e311d95cc7a85c0
mwoz123 <mwoz123@github.com>
2019-03-06 23:18:10 +0100

    refactor: create epubData; no sanitarizing

--- src/input/urlInputProcessor.js
+++ src/input/urlInputProcessor.js
@@ -8,21 +8,27 @@ const { tidy } = require('htmltidy2');
 module.exports = class UrlInputProcessor {
 
 
-    async gatherEbookData(urls, data, errors) {
+    async gatherEbookData(urls, ebookData, errors) {
 
         return await Promise.all(urls.map(async (url) => {
             console.log(`Processing: ${url}`);
             try {
-                const response = await axios.get(url);
+                const response = await axios.get(url); //TODO add {auth}
                 const dom = new JSDOM(response.data);
                 const docTitle = dom.window.document.title;
 
-                this.addAdditionalContent(dom.window.document, data, url);
 
-                this.ifNoOutputnameAndSingleUrlThenUseHtmlTitleAsFilename(urls, data, docTitle);
+                const imgs = dom.window.document.querySelectorAll("img");
+                imgs.forEach(e => console.log(e.src))
+
+
+                this.ifNoOutputnameAndSingleUrlThenUseHtmlTitleAsFilename(urls, ebookData, docTitle);
 
                 const cleanedHtml = await this.sanitarizeData(url, response);
-                data.content.push({
+
+                this.addAdditionalContent(cleanedHtml, ebookData, url);
+
+                ebookData.content.push({
                     title: docTitle,
                     data: cleanedHtml,
                     author: url
@@ -34,36 +40,35 @@ module.exports = class UrlInputProcessor {
         }));
     }
 
-    addAdditionalContent(document, data, url) {
+    async addAdditionalContent(html, ebookData, url) {
+
+        const dom = new JSDOM(html);
+
         const elements = new Map();
-        const imgs = document.querySelectorAll("img");
+        const imgs = dom.window.document.querySelectorAll("img");
         const allreadyProcessing = [];
 
-        imgs.forEach(img => {
+        await imgs.forEach(img => {
             if (img.src && !allreadyProcessing.includes(img.src)) {
                 console.log("Processing img:" + img.src)
                 allreadyProcessing.push(img.src);
 
-                axios({
-                    method: 'get',
-                    url: img.src,
+                axios.get(img.src, {
                     responseType: 'stream'
                 }).then((imgResp) => {
-                        if (imgResp.status === 200) {
+                    const name = this.extractFilename(img.src);
+                    const fs = require('fs');
+                    const consumer = fs.createWriteStream("./data/" + name);
+                    imgResp.data.pipe(consumer);
 
-                            // const name = this.extractFilename(img.src);
-                            // const consumer = fs.createWriteStream("./data/" + name);
-                            // imgResp.data.pipe(consumer);
-
-                            elements.set(img.src, imgResp.data)
-                        }
-                    });
+                    elements.set(img.src, imgResp.data)
+                });
             } else {
                 console.log("Allready processing: " + img.src);
             }
         });
-        data[url] = [];
-        data[url].push(elements);
+        ebookData[url] = [];
+        ebookData[url].push(elements);
     }
 
 
@@ -72,35 +77,35 @@ module.exports = class UrlInputProcessor {
     }
 
 
-    ifNoOutputnameAndSingleUrlThenUseHtmlTitleAsFilename(urls, data, docTitle) {
-        if (urls.length == 1 && !data.outputname) {
-            data.title = docTitle;
+    ifNoOutputnameAndSingleUrlThenUseHtmlTitleAsFilename(urls, ebookData, docTitle) {
+        if (urls.length == 1 && !ebookData.outputname) {
+            ebookData.title = docTitle;
         }
     }
 
 
 
     async sanitarizeData(url, response) {
-        const location = URL.parse(url);
-        const site = `${location.protocol}//${location.host}`;
+
+        const site = this.getSite(url);
+
         let parsed = absolutify(response.data, site);
 
         parsed = parsed.replace(/src=\'\/\//gm, `src='http://`);
-
         parsed = parsed.replace(/src=\"\/\//gm, `src='http://`);
 
-        const cleanedHtml = await sanitizeHtml(parsed, {
-            allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'html', 'body', 'head', 'title', 'article', 'style']),
-            preserveDoctypes: true,
-            allowProtocolRelative: false,
-            exclusiveFilter: function (frame) {
-                return frame.tag === 'img' && !frame.attribs.src; //fix exception when empty <img /> 
-            }
-        });
+        // const cleanedHtml = await sanitizeHtml(parsed, {
+        //     allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img', 'html', 'body', 'head', 'title', 'article', 'style', 'link' , 'section' , 'doctype']),
+        //     // preserveDoctypes: true,
+        //     // allowProtocolRelative: false,
+        //     exclusiveFilter: function (frame) {
+        //         return frame.tag === 'img' && !frame.attribs.src; //fix exception when empty <img /> 
+        //     }
+        // });
 
 
         const validHtml = await new Promise((resolve, reject) => {
-            tidy(cleanedHtml, async (err, html) => {
+            tidy(parsed, {doctype: 'html5',hideComments: true,} async (err, html) => {
                 if (err) {
                     reject(err);
                 } else {
@@ -111,5 +116,10 @@ module.exports = class UrlInputProcessor {
         return validHtml;
     }
 
+
+    getSite(url) {
+        const location = URL.parse(url);
+        return `${location.protocol}//${location.host}`;
+    }
 
 }
