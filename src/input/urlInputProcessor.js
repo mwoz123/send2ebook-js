3c47e929b9ec12fdb879a5b921c6463bcf8a6688
mwoz123 <mwoz123@gitlab.com>
2019-03-12 16:11:57 +0100

    w.i.p: add img data to epub subject

--- src/input/urlInputProcessor.js
+++ src/input/urlInputProcessor.js
@@ -5,7 +5,7 @@ const absolutify = require('absolutify')
 const URL = require('url');
 const { tidy } = require('htmltidy2');
 const { of, Observable, from, bindCallback, Subject } = require("rxjs");
-const { tap, map, flatMap, combineLatest, zip, retry, switchMap, skip } = require("rxjs/operators");
+const { tap, map, flatMap, combineLatest, zip, retry, switchMap, skip, distinct, filter, toArray, catchError } = require("rxjs/operators");
 // const { create } = require("rxjs-spy");
 // const spy = create();
 
@@ -26,7 +26,11 @@ module.exports = class UrlInputProcessor {
             const responseData$ = url$.pipe(
                 flatMap(u => axios.get(u)), //TODO add {auth} //TODO check if cannot be replaced by JSDOM.from(url)
                 retry(3),
+                catchError(err =>
+                    console.error(err)
+                ),
                 map(resp => resp.data),
+
             );
 
             const dom$ = responseData$.pipe(
@@ -37,21 +41,27 @@ module.exports = class UrlInputProcessor {
             )
             // this.ifNoOutputnameAndSingleUrlThenUseHtmlTitleAsFilename(urls, ebookData, docTitle);  //TODO move it to send2ebook.js
 
-            //         await this.addAdditionalContent(cleanedHtml, chapterData);
+
             const sanitarized$ = this.sanitarizeData(url$, responseData$);
 
+            const imgs$ = this.addAdditionalContent(sanitarized$);
+            // imgs$.pipe(toArray()).subscribe(e => {
+            //     console.log(e);
+            // });
+
             const chapterData$ = sanitarized$.pipe(
-                zip(title$, url$),
+                zip(title$, url$, imgs$.pipe(toArray)),
                 map(arr => {
                     return {
                         data: arr[0],
                         title: arr[1],
                         source: arr[2],
+                        extraElements: arr[3]
                     }
-                })
+                }),
             )
 
-            chapterData$.subscribe(
+            chapterData$.subscribe( //TODO check if can be replace by chapterData$.subscribe(chapterDataSubject) or reverse
                 cd => {
                     chapterDataSubject.next(cd);
                     if (++i === urls.length) {
@@ -64,48 +74,106 @@ module.exports = class UrlInputProcessor {
         return chapterDataSubject;
     }
 
-    async addAdditionalContent(html, chapterData) {
-        const dom = new JSDOM(html);
+    addAdditionalContent(sanitarized$, chapterData) {
+        const dom$ = sanitarized$.pipe(
+            map(dom => new JSDOM(dom))
+        )
+        // const dom = new JSDOM(html);
 
-        chapterData.extraElements = new Map();
-        const allreadyProcessing = new Map();
+        // chapterData.extraElements = new Map();
+        // const allreadyProcessing = new Map();
 
         // imgs.forEach((img, index, array) => { //TODO find way to async update DOM 
-        await this.processImages(allreadyProcessing, chapterData, dom);
-    }
+        //     await this.processImages(allreadyProcessing, chapterData, dom);
+        // }
 
-    async processImages(allreadyProcessing, chapterData, dom) {
+        // async processImages(allreadyProcessing, chapterData, dom) {
 
-        const imgs = dom.window.document.querySelectorAll("img");
+        // const imgs = dom.window.document.querySelectorAll("img");
+        const allImages$ = dom$.pipe(
+            flatMap(dom => from(dom.window.document.querySelectorAll("img"))),
+            filter(img => !!img.src),
+            filter(img => ! img.src.startsWith("http://image/")), //FIXME fix absolutify to not change ' src="data:image/png;base64'..
+            filter(img => ! img.src.startsWith("https://image/")),
+            distinct(img => img.src)
+        );
 
-        for (let index = 0; index < imgs.length; index++) {
-            let img = imgs[index];
-            if (img.src && !allreadyProcessing.has(img.src)) {
-                console.log("Processing img: " + img.src);
-                const name = this.extractFilename(img.src);
-                allreadyProcessing.set(img.src, name);
-                await axios.get(img.src, {
-                    responseType: 'stream'
-                }).then((imgResp) => {
-                    chapterData.extraElements.set(name, imgResp.data);
-                    img.setAttribute("src", name);
-                }).catch(err => {
-                    console.log("Error processing img: " + img.src + " error: " + err);
-                });
-            }
-            else {
-                console.log("Allready processing: " + img.src);
-                const imgFileName = allreadyProcessing.get(img.src);
-                img.setAttribute("src", imgFileName);
-            }
-        }
+        const chapterImgSubject = new Subject();
+
+        allImages$.subscribe(
+            img => {
+                const img$ = of(img);
+                const imgSrc$ = img$.pipe(
+                    // tap(e => console.log(e.src)),
+                    map(img => img.src)
+                );
+                const fileWithoutPath$ = imgSrc$.pipe(
+                    map(this.extractFilename)
+                );
+                const imgStream$ = imgSrc$.pipe(
+                    flatMap(imgSrc => axios.get(imgSrc, {
+                        responseType: 'stream',
+                        // httpAgent: false
+                    })),
+                    retry(3),
+                    catchError(err =>
+                        console.error(err)
+                    ),
+                    map(resp => resp.data),
+                )
+                const filenameAndImgStream$ = fileWithoutPath$.pipe(
+                    zip(imgStream$, img$),
+                    tap(arr => {
+                        arr[2].src = arr[0];
+                        // console.log(arr);
+                        arr[2].setAttribute("src", arr[0]);
+                    }),
+                    map(arr => {
+                        return {
+                            fileName: arr[0],
+                            data: arr[1]
+                        }
+                    }),
+                    tap(console.log)
+                )
+                filenameAndImgStream$.subscribe(imgData =>
+                    chapterImgSubject.next(imgData)
+                );
+            },
+            console.error,
+            () => chapterImgSubject.complete()
+        );
+
+        return chapterImgSubject;
+
+        // for (let index = 0; index < imgs.length; index++) {
+        //     let img = imgs[index];
+        //     if (img.src && !allreadyProcessing.has(img.src)) {
+        //         console.log("Processing img: " + img.src);
+        //         const name = this.extractFilename(img.src);
+        //         allreadyProcessing.set(img.src, name);
+        //         await axios.get(img.src, {
+        //             responseType: 'stream'
+        //         }).then((imgResp) => {
+        //             chapterData.extraElements.set(name, imgResp.data);
+        //             img.setAttribute("src", name);
+        //         }).catch(err => {
+        //             console.log("Error processing img: " + img.src + " error: " + err);
+        //         });
+        //     }
+        //     else {
+        //         console.log("Allready processing: " + img.src);
+        //         const imgFileName = allreadyProcessing.get(img.src);
+        //         img.setAttribute("src", imgFileName);
+        //     }
+        // }
     }
 
 
     extractFilename(url) {
-        const path = require('path');
-        return path.basename(url);
-        // return path.replace(/^.*[\\\/]/, '');
+        // const path = require('path');
+        // return path.basename(url);
+        return url.replace(/^.*[\\\/]/, '').replace(/[?].+/, "");
     }
 
 
